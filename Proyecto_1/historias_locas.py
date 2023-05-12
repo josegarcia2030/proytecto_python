14af476440646e6de815d670facaa1deef2ed282
jose enrique garcia flores <67805028+josegarcia2030@users.noreply.github.com>
2023-05-12 11:37:24 -0400

    Se realizo el 1er proyecto

---
+++ Proyecto_1/historias_locas.py
@@ -0,0 +1,61 @@
+#Concatenar cadena de caracteres.
+#Supongamos que queremos crear una cadena que diga:
+#Aprende a programar con ______________________:
+
+
+'''organicazion = ''
+print(f'Aprende a programar con {organicazion}')
+
+
+adj = input('Adjetivo: ')
+verbo1 = input('Vervo: ')
+verbo2 = input('Verbo: ')
+sustantivo_plural = input('Sustantivo (plural): ')
+
+madlib = f'¡Programar es tan {adj}!
+            Siempre me emociona porque me encanta
+            {verbo1} problemas. ¡Aprende a {verbo2} con
+            freeCodeCamp y alcanza tus {sustantivo_plural}!'
+
+
+print(madlib)
+'''
+
+def historia_loca():
+
+    def problema_juego():
+        print('El juego consiste en poder rellenar los espacios en blancos')
+        print('===========================================================')
+
+    def mostrar_texto():
+        print('Programar es tan (___________).')
+        print('Siempre me emociono porque me encanta (__________) resolver.')
+        print('¡Aprende a (________) con freeCodeCamp y alcanza tus (_________)')
+
+    def adjetivo():
+        pass
+
+    def verbo():
+        pass
+
+    def sustantivo():
+        pass
+
+
+    #
+    problema_juego()
+    mostrar_texto()
+
+historia_loca()
+
+
+
+
+
+
+
+
+
+
+
+
